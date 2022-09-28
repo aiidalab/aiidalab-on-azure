73b0a6c0b1f9ff1fa94a3eeec362536e589f7d6b
Carl Simon Adorf <simon.adorf@epfl.ch>
2022-09-28 17:28:07 +0200

    Make the specification of the dns_zone variable truly optional.
    Closes #18.

--- template/{{hostname}}/modules/aiidalab/variables.tf
+++ template/{{hostname}}/modules/aiidalab/variables.tf
@@ -1,19 +1,29 @@
+# tflint-ignore: terraform_unused_declarations
 variable "dns_zone_resource_group_name" {
   description = "The resource group name of the DNS zone"
+  type        = string
 }
 
+# tflint-ignore: terraform_unused_declarations
 variable "dns_zone_name" {
   description = "The name of the DNS zone"
+  type        = string
 }
 
+# tflint-ignore: terraform_unused_declarations
 variable "aks_service_principal_app_id" {
   description = "The application ID of the service principal"
+  type        = string
 }
 
+# tflint-ignore: terraform_unused_declarations
 variable "aks_service_principal_client_secret" {
   description = "The client secret of the service principal"
+  type        = string
 }
 
+# tflint-ignore: terraform_unused_declarations
 variable "aks_service_principal_object_id" {
   description = "The object ID of the service principal"
+  type        = string
 }
